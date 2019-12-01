25d35adec6715497e0c98eb825807b6c73f175b8
Garroti <gabryelmacedo0@gmail.com>
2019-12-01 16:34:45 -0300

    Updates

---
+++ precache-manifest.a401070d3d102e64ba1f8d1d39a59e6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caac3fec76314e9ddb6c6ccda8e71078",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "de1306c21af7569af98b",
+    "url": "/monsters-rolodex/static/css/main.19d717cf.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "de1306c21af7569af98b",
+    "url": "/monsters-rolodex/static/js/main.4af6a96a.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
